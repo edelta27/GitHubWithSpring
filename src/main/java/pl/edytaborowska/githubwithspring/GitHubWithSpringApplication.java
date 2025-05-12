197e8fd10667e0ed5d9f3e105fccb3d3b4ae97f5
Edyta <edysiaborowska27@gmail.com>
2025-05-12 19:26:11 +0200

    first commit

---
+++ src/main/java/pl/edytaborowska/githubwithspring/GitHubWithSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.edytaborowska.githubwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubWithSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edytaborowska/githubwithspring/GitHubWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edytaborowska.githubwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
